3231489ef7d5c9550c28c3181312b0a67ad7d6fe
ccchiu <ccchiu@wisdomgarden.com>
2020-05-16 22:22:52 +0800

    TwoSum

---
+++ LeetCode/LeetCode/LeetCode.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.SqlServer.Server;
+
+namespace LeetCode
+{
+    public class LeetCode
+    {
+        public string TwoSum(int[] nums, int target, ref int time)
+        {
+            Stopwatch stopWatch = new Stopwatch();
+            stopWatch.Start();
+            //二者相加
+            //for (int i = 0; i < nums.Length; i++)
+            //{
+            //    for (int j = 0; j < nums.Length; j++)
+            //    {
+            //        if (i == j) continue;
+
+            //        if (nums[i] + nums[j] == target)
+            //        {
+            //            stopWatch.Stop();
+            //            time = stopWatch.Elapsed.Milliseconds;
+            //            return $"{i},{j}";
+            //        }
+            //    }
+            //}
+
+            //項目清單.index(總數 - 項目)
+            List<int> remain = new List<int>();
+            for (int i = 0; i < nums.Length; i++)
+            {
+                var key = target - nums[i];
+                if (remain.Any(o => o == nums[i]))
+                {
+                    return $"{remain.IndexOf(nums[i])},{i}";
+                }
+                remain.Add(key);
+            }
+            stopWatch.Stop();
+            time = stopWatch.Elapsed.Seconds;
+            return "not find";
+        }
+
+      
+    }
+}
---
+++ LeetCode/UnitTestProject/UnitTest1.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using LeetCode;
+
+namespace UnitTestProject
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        private LeetCode.LeetCode code = new LeetCode.LeetCode();
+        [TestMethod]
+        public void TwoSum()
+        {
+            int time = 0;
+            string actual = code.TwoSum(new int[] {1,3,9,4},7,ref time);
+
+            Assert.AreEqual("1,3", actual);
+            Console.WriteLine(time);
+        }
+    }
+}
