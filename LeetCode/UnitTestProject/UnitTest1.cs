5abd371b2c766823a7599d43e83d3565550bf5d3
Linkindias <Linkindias21617@gmail.com>
2020-05-20 11:29:55 +0800

    Add scenario For Tdd

--- LeetCode/UnitTestProject/UnitTest1.cs
+++ LeetCode/UnitTestProject/UnitTest1.cs
@@ -8,6 +8,27 @@ namespace UnitTestProject
     public class UnitTest1
     {
         private LeetCode.LeetCode code = new LeetCode.LeetCode();
+
+        [TestMethod]
+        public void TwoSumInputNull()
+        {
+            int time = 0;
+            string actual = code.TwoSum(null, 7, ref time);
+
+            Assert.AreEqual("", actual);
+            Console.WriteLine(time);
+        }
+
+        [TestMethod]
+        public void TwoSumNotFind()
+        {
+            int time = 0;
+            string actual = code.TwoSum(new int[] { 1, 3, 5, 7 }, 7, ref time);
+
+            Assert.AreEqual("not find", actual);
+            Console.WriteLine(time);
+        }
+
         [TestMethod]
         public void TwoSum()
         {
@@ -29,12 +50,42 @@ namespace UnitTestProject
         }
 
         [TestMethod]
+        public void ReverseIlntegerOverPower()
+        {
+            int time = 0;
+            int actual = code.ReverseInteger(65539, ref time);
+
+            Assert.AreEqual(0, actual);
+            Console.WriteLine(time);
+        }
+
+        [TestMethod]
+        public void PalindromeNumberSingleInput()
+        {
+            int time = 0;
+            bool actual = code.PalindromeNumber(0, ref time);
+
+            Assert.AreEqual(false, actual);
+            Console.WriteLine(time);
+        }
+
+        [TestMethod]
         public void PalindromeNumber()
         {
             int time = 0;
-            bool actual = code.PalindromeNumber(-123, ref time);
+            bool actual = code.PalindromeNumber(10, ref time);
 
             Assert.AreEqual(false, actual);
+            Console.WriteLine(time);
+        }
+
+        [TestMethod]
+        public void RomantoIntegerNotMap()
+        {
+            int time = 0;
+            int actual = code.RomantoInteger("ABMCM", ref time);
+
+            Assert.AreEqual(0, actual);
             Console.WriteLine(time);
         }
 
@@ -45,6 +96,26 @@ namespace UnitTestProject
             int actual = code.RomantoInteger("MCMXCIV", ref time);
 
             Assert.AreEqual(1994, actual);
+            Console.WriteLine(time);
+        }
+
+        [TestMethod]
+        public void LongestCommonPrefixInputNull()
+        {
+            int time = 0;
+            string actual = code.LongestCommonPrefix(null, ref time);
+
+            Assert.AreEqual("", actual);
+            Console.WriteLine(time);
+        }
+
+        [TestMethod]
+        public void LongestCommonPrefixNotCommon()
+        {
+            int time = 0;
+            string actual = code.LongestCommonPrefix(new[] { "abc", "edf", "uyio" }, ref time);
+
+            Assert.AreEqual("", actual);
             Console.WriteLine(time);
         }
 
