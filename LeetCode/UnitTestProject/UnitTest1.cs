46d65eeaf911d8763ca545fded47cc28e5569dc2
ccchiu <ccchiu@wisdomgarden.com>
2020-07-13 23:23:46 +0800

    Initial Project

---
+++ LeetCode/UnitTestProject/UnitTest1.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace UnitTestProject
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestInitialize]
+        public void TestInitial()
+        {
+        }
+
+        [ClassCleanup]
+        public void TestCleanup()
+        {
+        }
+
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
