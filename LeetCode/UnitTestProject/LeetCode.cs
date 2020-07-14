0ad371e398a5b841312d89fbd07838025ea03244
Linkindias <Linkindias21617@gmail.com>
2020-07-14 15:09:00 +0800

    Add TwoSum Method、UnitTest

---
+++ LeetCode/UnitTestProject/LeetCode.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace UnitTestProject
+{
+    public class LeetCode
+    {
+        public string TwoSum(int[] ints, int target)
+        {
+            if (ints.Length == 0) return "";
+
+            var lsRemain = new List<int>();
+            for (var i = 0; i < ints.Length; i++)
+            {
+                var key = target - ints[i];
+                if (lsRemain.Any(o => o == ints[i])) return $"{lsRemain.IndexOf(ints[i])},{i}";
+                lsRemain.Add(key);
+            }
+
+            return "Not Find";
+        }
+    }
+}
--- LeetCode/UnitTestProject/UnitTest1.cs
+++ LeetCode/UnitTestProject/UnitTest1.cs
@@ -5,11 +5,24 @@ using Microsoft.VisualStudio.TestTools.UnitTesting;
 namespace UnitTestProject
 {
     [TestClass]
-    public class UnitTest1
+    public class LeetCodeTest
     {
+        private LeetCode _leetCode = new LeetCode();
+
         [TestMethod]
-        public void TestMethod1()
+        public void TwoSumInputNull()
         {
+            TwoSumShouldBe("", new int[0], 0);
+        }
+
+        [TestMethod]
+        public void TwoSum()
+        {
+            TwoSumShouldBe("1,3", new int[] {1,2,3,4,5}, 6);
+        }
+        private void TwoSumShouldBe(string expected, int[] ints, int target)
+        {
+            Assert.AreEqual(expected, _leetCode.TwoSum(ints,target));
         }
     }
 }
