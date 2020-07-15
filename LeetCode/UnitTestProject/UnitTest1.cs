9e3e87e77db8c9e8fb7bdb8bd1fe235b6ed691b1
Linkindias <Linkindias21617@gmail.com>
2020-07-15 09:40:44 +0800

    Initial Project

---
+++ LeetCode/UnitTestProject/UnitTest1.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace UnitTestProject
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void MyTestMethod()
+        {
+
+        }
+    }
+}
